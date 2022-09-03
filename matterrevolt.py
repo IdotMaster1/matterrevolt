c6b136bd479ef51d66f3d1dc84f062a69907777c
idotmaster1 <justapersom@gmail.com>
2022-09-03 18:08:40 +0200

    push broken and bad code

--- matterrevolt.py
+++ matterrevolt.py
@@ -11,6 +11,11 @@ def sendMsg(username, gateway, text):
     payload = {"text": text,"username":username ,"gateway":gateway}
     r = requests.post('http://localhost:4242/api/message', data=json.dumps(payload), headers=headers)
 
+def recvMsg():
+    while True:
+     h = requests.get('http://localhost:4242/api/messages').json()
+     return str(h)
+
 sendMsg("Revolt", "gateway1", "Bot is ready!")
 
 @pyrevolt.ReadySimplified()
@@ -22,8 +27,12 @@ async def onMessage(message: pyrevolt.Message) -> None:
  print(f"{message.author.username} said: {message.content}")
  sendMsg(message.author.username, "gateway1", message.content)
 
+@bot.commands.Command(name="message")
+async def message(message: pyrevolt.Message) -> None:
+    await message.Send(recvMsg)
+
 @bot.commands.Command(name="ping")
 async def ping(message: pyrevolt.Message) -> None:
     await message.Send(content=f"Pong {message.author.username}!", embed=pyrevolt.Embed.Create(title="Pong!", description=f"{message.author.mention}!", colour="#0000ff"), replies=[pyrevolt.Reply(message.messageID, True)])
 
-bot.Run(token="niggies")
+bot.Run(token="NN4d5EKY9KK7gVijp2zbn2Om6gmvm3MB7cnP54H5rfXY0zgSj_I2Qhg0PRGTti99")
